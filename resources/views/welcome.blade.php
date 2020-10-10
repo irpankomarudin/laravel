da5f1a490ae0ea1bedd89007a0edd8d6cb05997c
irpankomarudin <irpankomarudin10@gmail.com>
2020-10-10 10:30:25 +0700

    Edit Ke-3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Irpan
                 </div>
 
                 <div class="links">
